66e8a31fe541ac4ef87c9d13bd0252d14f61b0db
Derick Lwelamila <dericklwelamila@gmail.com>
2026-01-11 23:32:45 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5033465422-2cfe5",
+  "appId": "1:628973658368:web:3a09155a070dc02ab493e5",
+  "apiKey": "AIzaSyDRaoNPlu15RwFdurNWodOQ-VutsgpijZo",
+  "authDomain": "studio-5033465422-2cfe5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "628973658368"
+};
