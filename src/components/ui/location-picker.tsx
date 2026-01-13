35e4b1fec8d8dbeb415784a91a6c157af51144cd
Derick Lwelamila <dericklwelamila@gmail.com>
2026-01-13 22:28:26 +0000

    Try fixing this error: `Runtime Error: Map container is already initiali

--- src/components/ui/location-picker.tsx
+++ src/components/ui/location-picker.tsx
@@ -1,67 +1,24 @@
 'use client';
 
 import { useState, useRef, useMemo } from 'react';
-import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
-import type { LatLngExpression, LatLng } from 'leaflet';
+import dynamic from 'next/dynamic';
+import type { LatLngExpression } from 'leaflet';
 import { Skeleton } from './skeleton';
-import { ClientOnly } from '../client-only';
 
 interface LocationPickerProps {
   onLocationChange: (lat: number, lng: number) => void;
 }
 
-const DraggableMarker = ({ onLocationChange }: LocationPickerProps) => {
-  const [position, setPosition] = useState<LatLngExpression>([-6.792354, 39.208328]);
-  const markerRef = useRef<any>(null);
-
-  const map = useMapEvents({
-    click(e) {
-      setPosition(e.latlng);
-      onLocationChange(e.latlng.lat, e.latlng.lng);
-      map.flyTo(e.latlng, map.getZoom());
-    },
-  });
-
-  const eventHandlers = useMemo(
-    () => ({
-      dragend() {
-        const marker = markerRef.current;
-        if (marker != null) {
-          const newPos = marker.getLatLng();
-          setPosition(newPos);
-          onLocationChange(newPos.lat, newPos.lng);
-        }
-      },
-    }),
-    [onLocationChange]
-  );
-
-  return (
-    <Marker
-      draggable={true}
-      eventHandlers={eventHandlers}
-      position={position}
-      ref={markerRef}
-    />
-  );
-};
+// Dynamically import the map component itself to ensure it's client-side only
+const MapWithMarker = dynamic(() => import('./map-with-marker'), {
+  ssr: false,
+  loading: () => <Skeleton className="h-[400px] w-full" />,
+});
 
 export default function LocationPicker({ onLocationChange }: LocationPickerProps) {
-    return (
-      <ClientOnly>
-        <MapContainer
-            center={[-6.792354, 39.208328]}
-            zoom={13}
-            scrollWheelZoom={false}
-            style={{ height: '400px', width: '100%', borderRadius: 'var(--radius)' }}
-            placeholder={<Skeleton className="h-full w-full" />}
-        >
-            <TileLayer
-                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <DraggableMarker onLocationChange={onLocationChange} />
-        </MapContainer>
-      </ClientOnly>
-    );
+  return (
+    <div className="h-[400px] w-full rounded-lg overflow-hidden">
+        <MapWithMarker onLocationChange={onLocationChange} />
+    </div>
+  );
 }
---
+++ src/components/ui/map-with-marker.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import { useState, useRef, useMemo } from 'react';
+import { MapContainer, TileLayer, Marker, useMapEvents } from 'react-leaflet';
+import type { LatLngExpression, LatLng } from 'leaflet';
+
+interface MapWithMarkerProps {
+  onLocationChange: (lat: number, lng: number) => void;
+}
+
+const DraggableMarker = ({ onLocationChange }: MapWithMarkerProps) => {
+  const [position, setPosition] = useState<LatLngExpression>([-6.792354, 39.208328]);
+  const markerRef = useRef<any>(null);
+
+  const map = useMapEvents({
+    click(e) {
+      setPosition(e.latlng);
+      onLocationChange(e.latlng.lat, e.latlng.lng);
+      map.flyTo(e.latlng, map.getZoom());
+    },
+  });
+
+  const eventHandlers = useMemo(
+    () => ({
+      dragend() {
+        const marker = markerRef.current;
+        if (marker != null) {
+          const newPos = marker.getLatLng();
+          setPosition(newPos);
+          onLocationChange(newPos.lat, newPos.lng);
+        }
+      },
+    }),
+    [onLocationChange]
+  );
+
+  return (
+    <Marker
+      draggable={true}
+      eventHandlers={eventHandlers}
+      position={position}
+      ref={markerRef}
+    />
+  );
+};
+
+
+export default function MapWithMarker({ onLocationChange }: MapWithMarkerProps) {
+    return (
+        <MapContainer
+            center={[-6.792354, 39.208328]}
+            zoom={13}
+            scrollWheelZoom={false}
+            style={{ height: '100%', width: '100%' }}
+        >
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <DraggableMarker onLocationChange={onLocationChange} />
+        </MapContainer>
+    );
+}
